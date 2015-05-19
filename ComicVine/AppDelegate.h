9ae8f40a8eded239d3f2b14c9b2cfeaf28602da7
alejo80805 <alejo80805@hotmail.com>
2015-05-18 20:14:29 -0500

    Initial Commit

---
+++ ComicVine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ComicVine
+//
+//  Created by Alejandro Rodriguez on 5/18/15.
+//  Copyright (c) 2015 Alejandro Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
